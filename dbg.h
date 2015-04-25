9f6474b91b32e9b3e9694357ca26ff960c889744
Colin Woodbury <colingw@gmail.com>
2015-04-25 11:54:13 -0700

    (dbg) Changed how current filename is calculated
    - Previously, the entire path of the file was being printed, which
      would take up most of the screen. Now it only shows the filename itself.

--- dbg.h
+++ dbg.h
@@ -6,23 +6,26 @@
  */
 
 #include <stdio.h>
+#include <string.h>
 #include <errno.h>
 #include <string.h>
 #include "colour.h"
 
+#define __FILENAME__ (strrchr(__FILE__, '/') ? strrchr(__FILE__, '/') + 1 : __FILE__)
+
 #ifdef NDEBUG
 #define debug(M, ...)
 #else
-#define debug(M, ...) fprintf(stderr, "[" ANSI_MAGENTA "DEBUG" ANSI_RESET "] (%s:%d:%s) " M "\n", __FILE__, __LINE__, __func__, ##__VA_ARGS__)
+#define debug(M, ...) fprintf(stderr, "[" ANSI_MAGENTA "DEBUG" ANSI_RESET "] (%s:%d:%s) " M "\n", __FILENAME__, __LINE__, __func__, ##__VA_ARGS__)
 #endif
 
 #define clean_errno() (errno == 0 ? "None" : strerror(errno))
 
-#define log_err(M, ...) fprintf(stderr, "[" ANSI_RED "ERROR" ANSI_RESET "] (%s:%d:%s: errno: %s) " M "\n", __FILE__, __LINE__, __func__, clean_errno(), ##__VA_ARGS__)
+#define log_err(M, ...) fprintf(stderr, "[" ANSI_RED "ERROR" ANSI_RESET "] (%s:%d:%s: errno: %s) " M "\n", __FILENAME__, __LINE__, __func__, clean_errno(), ##__VA_ARGS__)
 
-#define log_warn(M, ...) fprintf(stderr, "[" ANSI_YELLOW "WARN" ANSI_RESET " ] (%s:%d:%s: errno: %s) " M "\n", __FILE__, __LINE__, __func__, clean_errno(), ##__VA_ARGS__)
+#define log_warn(M, ...) fprintf(stderr, "[" ANSI_YELLOW "WARN" ANSI_RESET " ] (%s:%d:%s: errno: %s) " M "\n", __FILENAME__, __LINE__, __func__, clean_errno(), ##__VA_ARGS__)
 
-#define log_info(M, ...) fprintf(stderr, "[" ANSI_CYAN "INFO" ANSI_RESET " ] (%s:%d:%s) " M "\n", __FILE__, __LINE__, __func__, ##__VA_ARGS__)
+#define log_info(M, ...) fprintf(stderr, "[" ANSI_CYAN "INFO" ANSI_RESET " ] (%s:%d:%s) " M "\n", __FILENAME__, __LINE__, __func__, ##__VA_ARGS__)
 
 #define check(A, M, ...) if(!(A)) { log_err(M, ##__VA_ARGS__); errno=0; goto error; }
 
